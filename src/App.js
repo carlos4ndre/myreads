2013a9a4276ca436c52536885b6dd4d379b54fd1
Carlos André <carlos.ulrich@gmail.com>
2017-10-27 11:30:50 +0100

    Fetch books after mounting BooksApp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
+import * as BooksAPI from './BooksAPI'
 import ShowSearchPage from './components/ShowSearchPage'
 import HomePage from './components/HomePage'
 import { Switch, Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState({ books })
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
-      <div className="app">
+      <div className='app'>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/search' component={ShowSearchPage} />
